32a09fd2414eae486a0d3311a311c222dde57531
Shareef-deen <49625061+Shareef-deen@users.noreply.github.com>
2022-01-12 13:52:41 +0000

    Update server.js

--- server.js
+++ server.js
@@ -24,10 +24,10 @@ app.get("/", function (req, res) {
   res.json({greeting: 'hello API'});
 });*/
 
-app.get("/api",(req, res){
+/*app.get("/api",(req, res){
   const date = new Date();
   res.json({unix: date.getTime(), utc: date.toUTCString()});
-        })
+        })*/
 
 app.get("/api/1451001600000", (req, res)=>{
   res.json({ unix: 1451001600000, utc: "Fri, 25 Dec 2015 00:00:00 GMT" });
