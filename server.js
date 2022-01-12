6badfe5f17c305e0dfe16ab493edfad53a971680
Shareef-deen <49625061+Shareef-deen@users.noreply.github.com>
2022-01-12 13:57:00 +0000

    Update server.js

--- server.js
+++ server.js
@@ -28,6 +28,10 @@ app.get("/", function (req, res) {
   const date = new Date();
   res.json({unix: date.getTime(), utc: date.toUTCString()});
         })*/
+app.get("/api",(res, req)=>{
+  const date = new Date();
+  res.json({unix: date.getTime(), utc: date.toUTCString()})
+})
 
 app.get("/api/1451001600000", (req, res)=>{
   res.json({ unix: 1451001600000, utc: "Fri, 25 Dec 2015 00:00:00 GMT" });
